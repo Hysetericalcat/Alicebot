5973f30d6e3bcbcdd7ef0ce5eb50ec34e891a445
akshat jain <chromeblood@akshats-MacBook-Air-3.local>
2026-03-22 21:15:43 +0530

    transformer block completed

--- utils.py
+++ utils.py
@@ -36,20 +36,44 @@ class Self_Attention(nn.Module):
 
 # Torch only looks at parts registered to torch       
 class MultiHeadAttention(nn.Module):
-     
      def __init__(self,n_head,d_model,d_k):
          super().__init__()
          self.W_0 = nn.Linear(d_model,d_model)
          self.heads = nn.ModuleList([Self_Attention(d_model, d_k) for _ in range(n_head)])
 
-              
-     
      def forward(self,x):
        outputs = [head(x) for head in self.heads]
        return  self.W_0(torch.cat(outputs, dim=-1)) 
              
-        
+class MLP(nn.Module):
+    def __init__(self,d_model):
+       super().__init__()
+       self.l1 = nn.Linear(d_model,4*d_model) #4*d_model = helps it learn complex transformation (more the value , more complex learning)
+       #so total parmaters of a model = 4*d_model*d_model
+       self.l2 = nn.Linear(4*d_model,d_model)
 
+   #activation between layers, never after the last one.
+    def forward(self,x):
+        x = F.gelu(self.l1(x))
+        x = self.l2(x)
+        return x
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self,n_head,d_model):
+        super().__init__()
+        self.mma = MultiHeadAttention(n_head,d_model,d_model//n_head) #// -> integer division
+        self.mlp = MLP(d_model)
+        #Layer norm implementation
+        self.ln1 = nn.LayerNorm(d_model)
+        self.ln2 = nn.LayerNorm(d_model)
+    
+    def forward(self,x):
+     #without residual connections,attention output completely replaces x(but attentions scores are garbage at start so it preserves it) :
+     x = x + self.mma(self.ln1(x))
+     x = x + self.mlp(self.ln2(x))   
+
+     return x
 
        
 
